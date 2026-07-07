58f25946f636a8c19255c02576ea5386867d637d
Ahmad David <davidk3476@gmail.com>
2026-07-07 09:14:27 +0700

    Update ForgotPasswordPage

--- frontend/src/pages/ForgotPassword/ForgotPasswordForm.tsx
+++ frontend/src/pages/ForgotPassword/ForgotPasswordForm.tsx
@@ -1,6 +1,6 @@
 import { useState } from "react";
 import { Mail } from "lucide-react";
-import { Link, useNavigate } from "react-router-dom";
+import { Link } from "react-router-dom";
 import { toast } from "sonner";
 
 import Button from "../../components/ui/Button";
